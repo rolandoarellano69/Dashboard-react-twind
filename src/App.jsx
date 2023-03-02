e26a587a4179fc00087710ba4433423cd469137e
Rolando <rolandoarellano9869@hotmail.com>
2023-03-02 15:53:03 -0800

    MenuMovil 0.1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import Sidebar from "./components/shared/Sidebar"
-import {RiMenu2Fill} from "react-icons/ri"
+import { useState } from "react"
+import { RiMenu2Fill, RiHome3Line, RiFileList3Line, RiSettings4Line } from "react-icons/ri"
+import { CgCoffee } from "react-icons/cg"
 function App() {
-
+const [showMenu, setShowMenu] = useState(false)
+const [showOrder, setShowOrder] = useState(false)
 
   return (
     <div className="App bg-[#262837] w-full min-h-screen ">
 
       <Sidebar />
       {/*menu movil*/}
-    <nav className=" bg-[#1f1d2b] lg:hidden ">  </nav>
-      <button>
-        <RiMenu2Fill />
-      </button>
-      <button>
-        <RiMenu2Fill />
-      </button>
-      <button>
-        <RiMenu2Fill />
-      </button>
-      <button>
-        <RiMenu2Fill />
-      </button>
+      <nav className=" bg-[#1f1d2b] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-500 p-6  flex items-center justify-between rounded-tl-xl rounded-tr-xl">
+        <button>
+          <RiHome3Line className="text-white "/>
+        </button>
+        <button>
+          <CgCoffee />
+        </button>
+        <button>
+          <RiFileList3Line />
+        </button>
+        <button>
+          <RiSettings4Line />
+        </button>
+      </nav>
     </div>
   )
 }
